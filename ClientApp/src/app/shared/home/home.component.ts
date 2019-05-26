31e009aecf232208cf374dacf23962b1af06534d
phucnguyencr <vanphuccr@gmail.com>
2019-05-26 18:16:04 +0700

    update home, contact, about

--- ClientApp/src/app/shared/home/home.component.ts
+++ ClientApp/src/app/shared/home/home.component.ts
@@ -8,10 +8,16 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class SharedHomeComponent implements OnInit {
   searchText = '';
+  homeText = '';
   constructor(private router: Router) { }
 
   ngOnInit() {
-    // this.id = this.route.snapshot.paramMap.get('id');
+    this.homeText = `<span class="font-large text-blue"><em>VOSA</em></span>
+    <span class="text-blue">Distribution Logistics Solution</span>
+    <span class="text-blue">(</span><span class="font-large text-blue"><em>VOSA</em></span>
+    <span class="text-blue">DLS</span><span class="text-blue">)</span>
+    was split from forwarding department of mother companies since 2014
+    to specialize in solutions for Logistics Distribution and Supply Chain Management.`;
   }
 
   search() {
